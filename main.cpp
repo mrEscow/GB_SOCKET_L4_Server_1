8c93653b478d9ef1e423e2164993dbef903708ca
mr.Escow <escowxbox@gmail.com>
2022-04-27 22:08:15 +0300

    Create class Transceiver

--- main.cpp
+++ main.cpp
@@ -10,60 +10,163 @@
 #include <QDebug>
 #include <thread>
 
+#include <algorithm>
+#include <cassert>
+#include <filesystem>
+#include <fstream>
+#include <future>
+//#include <iostream>
+//#include <list>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
+#include <cerrno>
 
-//bool send_buffer(const std::vector<char> &buffer,int index)
-//{
-//    size_t transmit_bytes_count = 0;
+//const auto clients_count = 10;
+const auto buffer_size = 4096;
+using namespace std::literals;
+namespace fs = std::filesystem;
 
-//    const auto size = buffer.size();
+//------------------------------------------------------------------------------------
+class Transceiver{
+public:
 
-//    while (transmit_bytes_count != size)
-//    {
-//        auto result = send(
-//                    Connections[index],
-//                    &(buffer.data()[0]) + transmit_bytes_count,
-//                    size - transmit_bytes_count,
-//                    NULL);
-
-//            transmit_bytes_count += result;
-
-//    }
-//    return true;
-//}
+    Transceiver(SOCKET &&client_sock) :
+    client_sock_(std::move(client_sock)) {}
+    Transceiver() = delete;
 
 
+public:
+    bool send_buffer(const std::vector<char> &buffer)
+    {
+        size_t transmit_bytes_count = 0;
+        const auto size = buffer.size();
+        while (transmit_bytes_count != size)
+        {
+            auto result = send(client_sock_, &(buffer.data()[0]) +
+            transmit_bytes_count,
+            size - transmit_bytes_count, 0);
+            if (-1 == result)
+            {
+                if (need_to_repeat()) continue;
+                return false;
+            }
+            transmit_bytes_count += result;
+        }
+        return true;
+    }
+
+    bool send_file(fs::path const& file_path)
+    {
+        std::vector<char> buffer(buffer_size);
+        std::ifstream file_stream(file_path);
+        if (!file_stream) return false;
+        qDebug() << "Sending file " << file_path.c_str() << "...\n";
+        while (file_stream)
+        {
+            file_stream.read(&buffer[0], buffer.size());
+            if (!send_buffer(buffer)) return false;
+        }
+        return true;
+    }
+
+    std::string get_request()
+    {
+        std::array<char, MAX_PATH + 1> buffer;
+        size_t recv_bytes = 0;
+        const auto size = buffer.size() - 1;
+        qDebug() << "Reading user request...\n";
+        while (true)
+        {
+            auto result = recv(client_sock_,
+            &buffer[recv_bytes], size - recv_bytes, 0);
+            if (!result) break;
+            if (-1 == result)
+            {
+                if (need_to_repeat()) continue;
+                throw std::logic_error("Socket reading error");
+            }
+            auto fragment_begin = buffer.begin() + recv_bytes;
+            auto ret_iter = std::find_if(fragment_begin,
+                fragment_begin + result,
+                [](char sym)
+                {
+                return '\n' == sym ||
+                '\r' == sym;
+                });
+            if (ret_iter != buffer.end())
+            {
+                *ret_iter = '\0';
+                recv_bytes += std::distance(fragment_begin, ret_iter);
+                break;
+            }
+            recv_bytes += result;
+            if (size == recv_bytes) break;
+        }
+        buffer[recv_bytes] = '\0';
+        auto result = std::string(buffer.begin(),
+        buffer.begin() + recv_bytes);
+        qDebug() << "Request = \"" << result.c_str() << "\"";
+        return result;
+    }
+
+private:
+    static bool need_to_repeat()
+    {
+        switch (errno)
+        {
+        case EINTR:
+        case EAGAIN:
+        // case EWOULDBLOCK: // EWOULDBLOCK == EINTR.
+        return true;
+        }
+        return false;
+    };
+private:
+    SOCKET client_sock_;
+
+};
+//------------------------------------------------------------------------------------
 class Client{
     QString m_name;
     SOCKET m_socket;
+    Transceiver m_transceiver;
 public:
-    Client(SOCKET socket): m_socket(socket){
+    Client(SOCKET socket)
+        : m_socket(socket),
+          m_transceiver(std::move(socket))
+    {
         QString msg = "Hello from Server!";
         int SizeMsg = msg.size();
 
         send(m_socket,(char*)SizeMsg,sizeof(int),NULL);
         send(m_socket, msg.toStdString().c_str(),SizeMsg, NULL);
     };
+
     Client() = delete;
 
     void SendMessage(const QString msg){
 
         int SizeMsg = msg.size();
-
+        qDebug() << "MSG:" << msg << "SIZE:" << SizeMsg;
         send(m_socket,(char*)SizeMsg,sizeof(int),NULL);
-        send(m_socket, msg.toStdString().c_str(),SizeMsg, NULL);
+        send(m_socket, msg.toStdString().c_str(), SizeMsg, NULL);
     };
 
     ~Client(){
        closesocket(m_socket);
     }
 };
-
+//------------------------------------------------------------------------------------
 class Server{
     SOCKET m_socket;
     SOCKADDR_IN m_addr;
     int m_addrSize;
-    QList<Client> m_Clients;
+    QList<Client*> m_Clients;
     bool m_stop{false};
+    std::thread thr;
 public:
     Server()= delete;
     Server(SOCKET socket,SOCKADDR_IN addr)
@@ -73,7 +176,6 @@ public:
         m_addrSize = sizeof(m_addr);
         bind(m_socket,(SOCKADDR*)&m_addr,sizeof(m_addr));
         listen(m_socket,SOMAXCONN);
-
     }
     ~Server(){
         m_stop = true;
@@ -87,28 +189,32 @@ public:
         std::thread thr([&](){
             while(!m_stop){
                 SOCKET newConnection = accept( m_socket ,(SOCKADDR*) &m_addr, &m_addrSize);
-                m_Clients << newConnection;
-                closesocket(newConnection);
+                m_Clients.push_back(new Client(newConnection));
+                //closesocket(newConnection);
             }
         });
 
-        thr.join();
+        thr.detach();
+    }
+    void Send_message(const QString msg){
+        for(auto client: m_Clients)
+            client->SendMessageW(msg);
     }
 };
-
+//------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     // init WSA for windows
-    WSAData wsaData;
-    WORD DllVersion = MAKEWORD(2,2);
+    WSAData wsaData; // информация об интерфейсах компутера
+    WORD DllVersion = MAKEWORD(2,2); // (от второй до второй вернсии) uint 16 x << 8 | y = 514
     if(WSAStartup(DllVersion, &wsaData) !=0){
         qDebug() << "Error: WSAStartup";
         return a.exec();
     }
 
-    // init IP port and famaly
+    // init IP, port and famaly
     SOCKADDR_IN addr;
     addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
     addr.sin_port = htons(1989);
@@ -119,6 +225,13 @@ int main(int argc, char *argv[])
 
     Server server(sListen, addr);
     server.Run_server();
+
+    while(true){
+        Sleep(1000);
+        server.Send_message("Test message!");
+    }
+
+
     // Cleanup winsock
     WSACleanup();
     qDebug()<<"exit";
